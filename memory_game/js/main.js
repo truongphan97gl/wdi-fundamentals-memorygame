cd8707145b67ed067973ab467c469ee81ba8ec38
truongphan97gl <32690298+truongphan97gl@users.noreply.github.com>
2019-05-21 20:39:19 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,6 @@
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king", cardFour = "king";
+console.log("User Flipped " + cardOne);
+console.log("User Flipped " + cardFour);
